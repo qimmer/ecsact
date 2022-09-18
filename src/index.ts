71c16601615ad797996bcc640e4096ebf8fc1cd6
Kim Johannsen <kijo@cego.dk>
2022-09-18 07:40:44 +0200

    Removed internal exports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,5 @@ export * from "@src/EntityManager";
 export * from "@src/IEntity";
 export * from "@src/IEntityQuery";
 export * from "@src/IEntityManager";
-export * from "@src/Util/JsonUtils";
-export * from "@src/Util/Assert";
 export * from "@src/ArchetypeManager";
 export * from "@src/IArchetype";
